3fc4548c5090b9a5f188c84223bbcdfbd2ff3d61
ahnafy <yasin011@morris.umn.edu>
2018-09-11 10:48:27 -0500

    All tests pass for process_client_logs @Koran023

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,4 +6,4 @@ directory=$1
 
 cd $directory/var/log
 
-cat secure* | grep "Failed password" | sed -r 's/invalid user //' | sed -r 's/([a-zA-Z]+) *([0-9]{1,2}) ([0-9]{1,2}):.*:.*Failed password for ([a-zA-Z0-9]+) from (.*) port [0-9]+ ssh2/\1 \2 \3 \4 \5/' | cat > discovery_failed_login_data.txt
+cat secure* | grep "Failed password" | sed -r 's/invalid user //' | sed -r 's/([a-zA-Z]+) *([0-9]{1,2}) ([0-9]{1,2}):.*:.*Failed password for ([a-zA-Z0-9]+) from (.*) port [0-9]+ ssh2/\1 \2 \3 \4 \5/' | cat > ../../failed_login_data.txt
