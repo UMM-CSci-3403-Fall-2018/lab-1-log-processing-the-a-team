8ba13a954212f6bda07151d60b4ace366039ef10
ahnafy <yasin011@morris.umn.edu>
2018-09-13 10:46:01 -0500

    Change implementation to include mktemp command

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,9 +2,16 @@
 
 directory=$1
 
-cat $directory/*/failed_login_data.txt | awk '{print $4}' | sort | uniq -c | sed -r 's/ *([0-9]+) ([a-zA-Z0-9]+)/data.addRow(\[\x27\2\x27, \1\]);/' > $directory/raw_username_data.html
+raw_username_data=mktemp
+
+cat $directory/*/failed_login_data.txt | \
+	awk '{print $4}' | sort | uniq -c | \
+	sed -r 's/ *([0-9]+) ([a-zA-Z0-9]+)/data.addRow(\[\x27\2\x27, \1\]);/' > \
+	$raw_username_data
 
 
-bin/wrap_contents.sh $directory/raw_username_data.html html_components/username_dist $directory/username_dist.html
+bin/wrap_contents.sh $raw_username_data html_components/username_dist $directory/username_dist.html
+
+rm $raw_username_data
 
 
