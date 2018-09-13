b06a728f623a7bd66c1d01f34c69fa3cfe8e257f
ahnafy <yasin011@morris.umn.edu>
2018-09-13 10:46:47 -0500

    Write based on create_username_dist.sh. All tests pass.

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 #TODO Write create_hours_dist.sh
+
+directory=$1
+
+raw_hours_data=mktemp
+
+cat $directory/*/failed_login_data.txt | \
+	awk '{print $3}' | sort | uniq -c | \
+	awk '{print "data.addRow([\x27" $2 "\x27, " $1 "]);" }' > \
+	$raw_hours_data
+
+bin/wrap_contents.sh $raw_hours_data html_components/hours_dist $directory/hours_dist.html
+
+rm $raw_hours_data
