eb606bee7db047eda3399242705f383e510d746f
Avery Koranda <koran023@morris.umn.edu>
2018-09-18 11:35:45 -0500

    add scripts for process_logs.sh functions

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-#TODO Write process_logs.sh
+here=`pwd`
+directory=`mktemp -d`
+
+for tar_file in "$@"
+do
+	tar -xzf $tar_file -C $directory/`basename $tar_file "_secure.tgz"`
+	bin/process_client_logs.sh $directory/`basename $tar_file "_secure.tgz"`
+done
+
+bin/create_username_dist.sh $directory
+bin/create_hours_dist.sh $directory
+bin/create_country_dist.sh $directory
+bin/assemble_report.sh $directory
+
+mv failed_login_summary.html $here
+
